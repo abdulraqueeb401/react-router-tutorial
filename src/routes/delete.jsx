e7bc9a5219d1f98928a6cc070d9ccb8169d8566a
Abdul Raqueeb <abdulraqueeb2000@gmail.com>
2024-08-25 03:09:22 +0530

    add a new delete route

---
+++ src/routes/delete.jsx
@@ -0,0 +1,9 @@
+import { deleteContact } from "../contact";
+import { redirect } from "react-router-dom";
+
+export async function action({ params }) {
+  const { contactId } = params;
+  const destroyed = await deleteContact(contactId);
+  if (!destroyed) throw new Error("Oh dang, delete wasn't destroyed.");
+  return redirect("/");
+}

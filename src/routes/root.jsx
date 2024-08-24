5eb12f3d8288135f805a88097e2c36a5643083af
Abdul Raqueeb <abdulraqueeb2000@gmail.com>
2024-08-25 03:41:51 +0530

    fix input field when navigating

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -7,12 +7,13 @@ import {
   useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contact";
+import { useEffect } from "react";
 
 export async function loader({ request }) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts, q };
 }
 
 export async function action() {
@@ -21,9 +22,12 @@ export async function action() {
 }
 
 export default function Root() {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
   console.log(contacts);
+  useEffect(() => {
+    document.getElementById("q").value = q;
+  }, [q]);
   return (
     <>
       <div id="sidebar">
@@ -36,6 +40,7 @@ export default function Root() {
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>

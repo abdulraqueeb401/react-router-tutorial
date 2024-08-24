af7e79cc692d6346393a4b7d4c693eaa9af3313b
Abdul Raqueeb <abdulraqueeb2000@gmail.com>
2024-08-24 22:24:51 +0530

    add a root route to the router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World!</div>,
+    element: <Root />,
   },
 ]);
 

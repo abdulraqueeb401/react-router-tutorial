4f8881cae07f3cc094c2535b4b982261b53612fe
Abdul Raqueeb <abdulraqueeb2000@gmail.com>
2024-08-25 11:33:59 +0530

    fix error message for delete route

--- src/routes/delete.jsx
+++ src/routes/delete.jsx
@@ -4,6 +4,6 @@ import { redirect } from "react-router-dom";
 export async function action({ params }) {
   const { contactId } = params;
   const destroyed = await deleteContact(contactId);
-  if (!destroyed) throw new Error("Oh dang, delete wasn't destroyed.");
+  if (!destroyed) throw new Error("Oh dang, contact wasn't destroyed.");
   return redirect("/");
 }
